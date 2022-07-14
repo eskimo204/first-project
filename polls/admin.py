2c533d3cf59c0c0df14c85a932c6c36792dd16b7
cwhong <cwhong21@gmail.com>
2022-07-14 21:34:00 +0900

    forth commit

--- polls/admin.py
+++ polls/admin.py
@@ -2,6 +2,21 @@ from django.contrib import admin
 from .models import Question,Choice
 
 # Register your models here.
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
 
-admin.site.register(Question)
-admin.site.register(Choice)
+    
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+admin.site.register(Question, QuestionAdmin)
--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,16 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import loader
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from django.utils import timezone
+from django.views import generic
 
 
 from .models import Question
 
+
 # Create your views here.
-def index(request):
+# def index(request):
     # 1. Basic view
     # return HttpResponse("Hello, world.")
 
@@ -24,14 +28,24 @@ def index(request):
     # return HttpResponse(template.render(context, request))
 
     # 4. Use the shortcut - render
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return render(request, 'polls/index.html',context)
+    # latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # context = {
+    #     'latest_question_list': latest_question_list,
+    # }
+    # return render(request, 'polls/index.html',context)
+
+    #5. Use the Generic View
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
 
-def detail(request, question_id):
+# def detail(request, question_id):
     # 1. Basic view
     # return HttpResponse("You're looking at question %s." % question_id)
 
@@ -43,11 +57,37 @@ def detail(request, question_id):
     # return render(request, 'polls/detail.html', {'question': question})
 
     # 3. Use the shortcut - get_object_or_404
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+    # question = get_object_or_404(Question, pk=question_id)
+    # return render(request, 'polls/detail.html', {'question': question})
 
-def results(request, question_id):
-    return HttpResponse(response % question_id)
+    #4. Use the Generic View
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+# def results(request, question_id):
+#     return HttpResponse(response%auestion_id)
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/results.html', {'question': question})
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def vote(request, question_id):
-    return HttpResponse("You're voing on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
